d2dd4a681ddd8389e2d8462eb2f45e16b71595d5
Meelenych <meelenych@gmail.com>
2023-02-02 14:46:29 +0100

    adds testimonials section content

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,10 @@
 import './main.css';
 import deliveryIcon from '../../images/bike-delivery-icon.svg';
+import starIcon from '../../images/8fab66317d6c35dc211a165dbc3735e15466ae68.png';
+import user1 from '../../images/08c5772c481233a30a46aeca552132053604e2a8.jpg';
+import user2 from '../../images/3ca412176fe4306326b04a78b51fa148c49f99c1.jpg';
+import user3 from '../../images/44dd2beb0c06094368ffbb7fa1843005cfdae174.jpg';
+import user4 from '../../images/b64f988f077b50ffdab0afee9de4d701e4a9d5da.jpg';
 
 export function Main() {
 	return (
@@ -124,7 +129,110 @@ export function Main() {
 						</ul>
 					</div>
 				</section>
-				<section className='testimonials'></section>
+				<section className='testimonials'>
+					<div className='container'>
+						<h2 className='testimonialsHeading'>Testimonials</h2>
+						<div className='testimonialsWrapper'>
+							<ul className='testimonialsList'>
+								<li className='testimonialsItem'>
+									<div className='testimonialsCardWrapper'>
+										<div className='testimonialsRating'>
+											<p>Rating</p>
+											<div>
+												<img
+													src={starIcon}
+													alt='starIcon'
+												/>
+											</div>
+										</div>
+										<div className='testimonialsProfile'>
+											<img
+												src={user1}
+												alt=''
+											/>
+											<p>Name</p>
+										</div>
+										<div className='testimonialsText'>
+											<p>Review text</p>
+										</div>
+									</div>
+								</li>
+
+								<li className='testimonialsItem'>
+									<div className='testimonialsCardWrapper'>
+										<div className='testimonialsRating'>
+											<p>Rating</p>
+											<div>
+												<img
+													src={starIcon}
+													alt='starIcon'
+												/>
+											</div>
+										</div>
+										<div className='testimonialsProfile'>
+											<img
+												src={user2}
+												alt=''
+											/>
+											<p>Name</p>
+										</div>
+										<div className='testimonialsText'>
+											<p>Review text</p>
+										</div>
+									</div>
+								</li>
+
+								<li className='testimonialsItem'>
+									<div className='testimonialsCardWrapper'>
+										<div className='testimonialsRating'>
+											<p>Rating</p>
+											<div>
+												<img
+													src={starIcon}
+													alt='starIcon'
+												/>
+											</div>
+										</div>
+										<div className='testimonialsProfile'>
+											<img
+												src={user3}
+												alt=''
+											/>
+											<p>Name</p>
+										</div>
+										<div className='testimonialsText'>
+											<p>Review text</p>
+										</div>
+									</div>
+								</li>
+
+								<li className='testimonialsItem'>
+									<div className='testimonialsCardWrapper'>
+										<div className='testimonialsRating'>
+											<p>Rating</p>
+											<div>
+												<img
+													src={starIcon}
+													alt='starIcon'
+												/>
+											</div>
+										</div>
+										<div className='testimonialsProfile'>
+											<img
+												src={user4}
+												alt=''
+											/>
+											<p>Name</p>
+										</div>
+										<div className='testimonialsText'>
+											<p>Review text</p>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</section>
 				<section className='about'></section>
 			</main>
 		</>
