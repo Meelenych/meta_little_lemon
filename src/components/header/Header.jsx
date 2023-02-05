57e30b313184f8c3a46d059a563b0b90a8fce862
Meelenych <meelenych@gmail.com>
2023-02-05 18:45:58 +0100

    logo link routing fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 import { Nav } from '../nav/Nav';
 import logo from '../../images/Logo.svg';
 import './header.css';
@@ -6,15 +8,15 @@ export function Header() {
 	return (
 		<div className='container'>
 			<header>
-				<a
-					className='logoLink'
-					href=''>
+				<NavLink
+					to='/'
+					className='logoLink'>
 					<img
 						className='logoImg'
 						src={logo}
 						alt='Little Lemon logo'
 					/>
-				</a>
+				</NavLink>
 
 				<Nav></Nav>
 			</header>
