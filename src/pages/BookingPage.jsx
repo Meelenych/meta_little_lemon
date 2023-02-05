19071d2ec1f4cb0331c57b9fb7a7706f121f18b2
Meelenych <meelenych@gmail.com>
2023-02-05 20:33:17 +0100

    adds functionality for a table reservation page

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,12 +1,12 @@
 import { Header } from '../components/header/Header';
-
+import { BookingForm } from '../components/bookingForm/BookingForm';
 import { Footer } from '../components/footer/Footer';
 
 export function BookingPage() {
 	return (
 		<>
 			<Header></Header>
-
+			<BookingForm></BookingForm>
 			<Footer></Footer>
 		</>
 	);
